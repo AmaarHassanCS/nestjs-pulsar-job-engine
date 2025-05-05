d25f19af30d4f1eeeb18efc6fd287d032be28bf8
AmaarHassanCS <amaarhassancs@gmail.com>
2025-05-05 14:16:30 +0500

    tests fix

--- apps/jobber-auth/src/app/users/users.service.spec.ts
+++ apps/jobber-auth/src/app/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        // we will mock the dependencies of the service we are testing
+        // in this way, database or prisma itself wont be engaged
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
